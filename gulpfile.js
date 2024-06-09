50a4c2d9e1822974e0159de1a412c88f862a56a8
Guilherme-Bomfin-Dev <151377148+Guilherme-Bomfin-Dev@users.noreply.github.com>
2024-06-09 15:22:00 -0300

    criando o hero e importanto a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,33 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
-function styles(){
+function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle:'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
 }
 
-
-exports.default = styles;
-exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
 }
+
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./src/images/**/*', images);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./dist/js/*.js').on('change', browserSync.reload); // Adicionando também arquivos JS caso haja necessidade
+}
+
+exports.default = gulp.parallel(styles, images);
+exports.watch = serve;
