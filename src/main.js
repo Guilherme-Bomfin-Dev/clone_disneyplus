7dbb14030190e1d15ef105e54746a6cadd465bf9
Guilherme-Bomfin-Dev <151377148+Guilherme-Bomfin-Dev@users.noreply.github.com>
2024-06-24 20:56:29 -0300

    desenvolvendo a faq

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
+    const questions = document.querySelectorAll('[data-faq-question]');
+
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(event) {
             const abaAlvo = event.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active');
             removeActiveClassFromButtons();
             event.target.classList.add('shows__tabs__button--is-active');
         });
     }
-});
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+})
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+    
+    elementoPai.classList.toggle(classe);
+
+}
 
 function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
